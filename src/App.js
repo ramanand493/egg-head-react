90e550ba9969996b74cf06846c6b6c2edd4aeccc
ramanand495 <ramanand495@gmail.com>
2017-06-22 09:14:15 +0530

    Childrens props

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import React from 'react'
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      txt:'this is the state txt'
-    }
-  }
 
-update(e){
-  this.setState({txt: e.target.value })
-}
   render(){
-    return(
-      <div>
-        <Widget update={this.update.bind(this)} />
-          <Widget update={this.update.bind(this)} />
-            <Widget update={this.update.bind(this)} />
-        <h1> { this.state.txt }</h1>
-      </div>
-    )
+    return <Button> <Heart /> React</Button>
   }
 }
 
-const Widget = (props) => <input type="text" onChange={ props.update } />
+ const Button = ( props ) => <button>{ props.children }</button>
+
+class Heart extends React.Component{
+  render(){
+    return <span>&hearts;</span>
+  }
+}
 
 export default App
