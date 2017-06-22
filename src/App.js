919ff9988a200de4a18195023121acba37bfb6bc
ramanand495 <ramanand495@gmail.com>
2017-06-22 11:27:06 +0530

    propType Validation props

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import React from 'react'
 class App extends React.Component {
 
   render(){
-    return <Button> <Heart /> React</Button>
+    return <Title text="The Text"/>
   }
 }
 
- const Button = ( props ) => <button>{ props.children }</button>
+const Title = (props) => <h1>Title : { props.text }</h1>
 
-class Heart extends React.Component{
-  render(){
-    return <span>&hearts;</span>
+Title.propTypes = {
+  text(props,propName,Component){
+    if(!(propName in props)){
+      return new Error('Missing ${ propName }')
+    }
+    if(props[propName].length<6){
+      return new Error(' ${ propName } is too shorts')
+    }
   }
 }
-
 export default App
