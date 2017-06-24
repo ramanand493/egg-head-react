da953c1df346583f5d9957ecdb59bd737582cdff
ramanand495 <ramanand495@gmail.com>
2017-06-24 13:27:16 +0530

    life cycle of react

--- src/App.js
+++ src/App.js
@@ -1,38 +1,55 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 class App extends React.Component {
-constructor(){
-  super();
-    this.state ={
-                a:'update me a',
-                b:'update me b'
-              }
+  constructor(){
+    super();
+    this.state = {val:0}
+    this.update =this.update.bind(this)
+  }
 
-}
 update(){
-  this.setState({
-      a:this.refs.a.value,
-      b:this.refs.b.value
-    })
+  this.setState({val:this.state.val + 1})
 }
 
+componentWillMount(){
+  console.log('componentWillMount')
+}
+
+componentDidMount(){
+  console.log('componentDidMount')
+}
+
+componentWillUnmount(){
+  console.log('componentWillunmount')
+}
+
+
+render(){
+   console.log('render')
+   return <button onClick={this.update}>{this.state.val}</button>
+}
+
+}
+
+
+
+class Wrappper extends React.Component {
+  mount(){
+    ReactDOM.render(<App />,document.getElementById('a'))
+  }
+  unmount(){
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+  }
   render(){
     return (
       <div>
-          <input type="text"
-            ref="node => this.a = node "
-            onChange={ this.update.bind(this)}
-          />
-        <h1> {this.state.a }</h1>
-        <hr/>
-            <input type="text"
-              ref="b"
-              onChange={ this.update.bind(this)}
-            />
-          <h1>{this.state.b }</h1>
+          <button onClick={this.mount.bind(this)}>Mount</button>
+          <button onClick={this.unmount.bind(this)} >UnMount</button>
+          <div id="a"></div>
       </div>
     )
   }
 }
 
-export default App
+export default Wrappper
